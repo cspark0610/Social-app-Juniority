112cdc656cf18cc1575e149b7eb5fc55cb126729
cspark0610 <cpark0610@gmail.com>
2021-03-24 13:27:15 -0300

    login y register term

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import UserProfile from "./componentes/userViews/UserProfile"
 import Login from "./componentes/login/Login"
 import Register from "./componentes/register/Register"
 import Home from "./componentes/home/Home"
-import Navbar from "./componentes/commons/Navbar"
-import Footer from "./componentes/commons/Footer"
+
 
 function App() {
   return (
     <>
-      <Navbar/>
+    
         <Switch>
         <Route path exact="/"><Home /></Route>
         <Route path="/login" component={Login} />
         <Route path="/userProfile" component={UserProfile} />
         <Route path="/register" component={Register} />
         </Switch>
-      <Footer/>
+      
     </>
   );
 }
