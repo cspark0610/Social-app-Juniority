cbbde660fc19a5ef0ae2d2c826f5c970710992c3
Walker Peredo <walkerperedo@gmail.com>
2021-03-30 11:28:13 -0400

    conflictos arreglados

--- src/componentes/login/Login.jsx
+++ src/componentes/login/Login.jsx
@@ -13,11 +13,10 @@ const Login = () => {
   const firebase = useFirebaseApp();
   const classes = useStyles();
   const dispatch = useDispatch();
-  const currentUser = useSelector(state => state.currentUser);
+  const currentUser = useSelector((state) => state.currentUser);
   const [formData, setFormData] = useState({ email: "", password1: "" });
-  const {  email, password1 } = formData;
+  const { email, password1 } = formData;
   const history = useHistory();
-
 
   const handleChange = (text) => (e) => {
     setFormData({ ...formData, [text]: e.target.value });
@@ -29,17 +28,23 @@ const Login = () => {
       .auth()
       .signInWithEmailAndPassword(email, password1)
       .then((user) => {
-        db.collection('user').where("id", "==", user.user.uid).get()
-            .then(doc => doc.forEach(data => {
-                dispatch(setCurrentUser(data.data()));
-                sessionStorage.setItem('currentUser', JSON.stringify(data.data()));
-                history.push('/');
-            }))
+        db.collection("user")
+          .where("id", "==", user.user.uid)
+          .get()
+          .then((doc) =>
+            doc.forEach((data) => {
+              dispatch(setCurrentUser(data.data()));
+              sessionStorage.setItem(
+                "currentUser",
+                JSON.stringify(data.data())
+              );
+              history.push("/");
+            })
+          );
       });
   };
 
   return (
-
     <form onSubmit={handleSubmit} style={{ textAlign: "center" }}>
       <Typography
         style={{
@@ -49,63 +54,6 @@ const Login = () => {
         }}
       >
         <span
-    <>
-    { currentUser ? history.push('/') : (
-      <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
-      <div className="max-w-6xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1">
-        <div className="lg:w1/2 xl:w5/12 p-6 sm:p-12">
-          <div className="mt-12 flex flex-col items-center">
-            <h1 className="text-4xl xl:text-3xl font-black"> LOGIN</h1>
-          </div>
-          <form
-            onSubmit={handleSubmit}
-            className="w-full flex-1 mt-8 text-indigo-800"
-          >
-            <div className="mx-auto max-w-xs relative">
-              <input
-                type="email"
-                placeholder="Email"
-                onChange={handleChange("email")}
-                value={email}
-                className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                onChange={handleChange("password1")}
-                value={password1}
-                className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-              />
-
-              <button
-                type="submit"
-                className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
-              >
-                {" "}
-                <i className="fas fa-sign-in-alt fa 1x w-6-ml-2" />{" "}
-                <span className="ml-3">LOG IN </span>{" "}
-              </button>
-              <a href="/password-recovery">Forgot Password?</a>
-            </div>
-            <div className="my-12 border-b text-center">
-              <div className="flex-1 leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
-                Or sign with social login <i className="fab fa-google w-10" />{" "}
-                <i className="fab fa-facebook w-10-ml-2" />
-              </div>
-            </div>
-            <div className="flex flex-col items-center">
-              <Link
-                to="/register"
-                className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5"
-              >
-                <i className="fas fa-user-plus fa 1x w-6-ml-2 text-indigo-500" />
-                <span className="ml-4">Are you new? Register</span>
-              </Link>
-            </div>
-          </form>
-        </div>
-        <div
-          className="flex-1 bg-indigo-100 text-center hidden lg:flex"
           style={{
             borderBottom: "solid 1px #3cb4e5",
             paddingBottom: "1%",
@@ -178,15 +126,8 @@ const Login = () => {
           <span className="ml-4">Are you new? Register</span>
         </Link>
       </div>
-
     </form>
-
-    </div>
-    )}
-    </>
-
   );
 };
-
 
 export default Login;
--- src/componentes/register/Register.jsx
+++ src/componentes/register/Register.jsx
@@ -8,6 +8,7 @@ import { db } from "../../firebase/firebase";
 import firbaseTime from "firebase";
 import Alert from "@material-ui/lab/Alert";
 import Login from "../login/Login.jsx";
+import { useSelector } from "react-redux";
 import {
   Box,
   Button,
@@ -31,20 +32,21 @@ const Register = () => {
     password1: "",
     password2: "",
   });
-  
+
   const [lengthError, setLengthError] = useState(false);
   const { fullName, email, password1, password2 } = formData;
   const [change, setChange] = useState(["primary", "secondary"]);
+  const currentUser = useSelector((state) => state.currentUser);
 
   const handleChange = (text) => (e) => {
     setFormData({ ...formData, [text]: e.target.value });
-    if(text === 'password1') {
-        if(password1.length < 6) {
-            setLengthError(true);
-        } else {
-            setLengthError(false);
-        };
-    };
+    if (text === "password1") {
+      if (password1.length < 6) {
+        setLengthError(true);
+      } else {
+        setLengthError(false);
+      }
+    }
   };
 
   const handleSubmit = (e) => {
@@ -70,169 +72,178 @@ const Register = () => {
     }
   };
   return (
-    <div className={classes.bg}>
-      <Grid
-        container
-        justify="center"
-        spacing={8}
-        className={classes.gridContainer}
-        alignItems="center"
-      >
-        <Grid
-          item
-          xs={12}
-          style={{
-            margin: 0,
-            padding: 0,
-            // border: "black solid 1px",
-            textAlign: "end",
-          }}
-        >
-          <Button
-            color={change[0]}
-            variant="contained"
-            onClick={() => {
-              setChange(["primary", "secondary"]);
-            }}
+    <>
+      {currentUser ? (
+        history.push("/")
+      ) : (
+        <div className={classes.bg}>
+          <Grid
+            container
+            justify="center"
+            spacing={8}
+            className={classes.gridContainer}
+            alignItems="center"
           >
-            Log in
-          </Button>
-          <Button
-            color={change[1]}
-            variant="contained"
-            onClick={() => {
-              setChange(["secondary", "primary"]);
-            }}
-          >
-            Register
-          </Button>
-        </Grid>
-        <Grid item lg={6} component={Box} display={{ xs: "none", lg: "block" }}>
-          <Typography variant="h4">Juniority</Typography>
-          <Typography
-            variant="h4"
-            color="primary"
-            style={{
-              fontWeight: 500,
-              marginTop: "5%",
-              marginBottom: "8%",
-              lineHeight: "120%",
-            }}
-          >
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          </Typography>
-          <Typography variant="body2">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-            vero repellendus quaerat dolores, ipsum numquam molestias ad?
-            Quaerat repudiandae laboriosam deleniti sequi, velit deserunt,
-            itaque tempora eius provident reiciendis totam?
-          </Typography>
-        </Grid>
-        <Grid item xs={12} lg={6}>
-          {change[0] === "primary" ? (
-            <Login />
-          ) : (
-            <form onSubmit={handleSubmit} style={{ textAlign: "center" }}>
-              <TextField
-                className={classes.textField}
-                id="outlined-name"
-                label="Name"
-                type="text"
-                variant="outlined"
-                placeholder="Full Name"
-                size="small"
-                onChange={handleChange("fullName")}
-                value={fullName}
-              />
-              <TextField
-                className={classes.textField}
-                id="outlined-search"
-                label="Email"
-                type="email"
-                variant="outlined"
-                placeholder="example@gmail.com"
-                size="small"
-                onChange={handleChange("email")}
-                value={email}
-              />
-              <TextField
-                className={classes.textField}
-                id="outlined-password-input"
-                label="Password"
-                type="password"
-                variant="outlined"
-                placeholder="Password"
-                size="small"
-                onChange={handleChange("password1")}
-                value={password1}
-              />
-              <TextField
-                className={classes.textField}
-                id="outlined-password-input"
-                label="Confirm Password"
-              <input
-
-                type="password"
-                variant="outlined"
-                size="small"
-                placeholder="Confirm Password"
-                onChange={handleChange("password2")}
-                value={password2}
-              />
-              <div
-                style={{
-                  marginTop: "1%",
+            <Grid
+              item
+              xs={12}
+              style={{
+                margin: 0,
+                padding: 0,
+                // border: "black solid 1px",
+                textAlign: "end",
+              }}
+            >
+              <Button
+                color={change[0]}
+                variant="contained"
+                onClick={() => {
+                  setChange(["primary", "secondary"]);
                 }}
               >
-                <Typography variant="caption" align="center">
-                  <div className=" border-b text-center">
-                    <div className=" flex-1 leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
-                      Or Register with email{" "}
-                      <i className="fab fa-google w-10" />{" "}
-                      <i className="fab fa-facebook w-10-ml-2" />
-                    </div>
+                Log in
+              </Button>
+              <Button
+                color={change[1]}
+                variant="contained"
+                onClick={() => {
+                  setChange(["secondary", "primary"]);
+                }}
+              >
+                Register
+              </Button>
+            </Grid>
+            <Grid
+              item
+              lg={6}
+              component={Box}
+              display={{ xs: "none", lg: "block" }}
+            >
+              <Typography variant="h4">Juniority</Typography>
+              <Typography
+                variant="h4"
+                color="primary"
+                style={{
+                  fontWeight: 500,
+                  marginTop: "5%",
+                  marginBottom: "8%",
+                  lineHeight: "120%",
+                }}
+              >
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </Typography>
+              <Typography variant="body2">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
+                vero repellendus quaerat dolores, ipsum numquam molestias ad?
+                Quaerat repudiandae laboriosam deleniti sequi, velit deserunt,
+                itaque tempora eius provident reiciendis totam?
+              </Typography>
+            </Grid>
+            <Grid item xs={12} lg={6}>
+              {change[0] === "primary" ? (
+                <Login />
+              ) : (
+                <form onSubmit={handleSubmit} style={{ textAlign: "center" }}>
+                  <TextField
+                    className={classes.textField}
+                    id="outlined-name"
+                    label="Name"
+                    type="text"
+                    variant="outlined"
+                    placeholder="Full Name"
+                    size="small"
+                    onChange={handleChange("fullName")}
+                    value={fullName}
+                  />
+                  <TextField
+                    className={classes.textField}
+                    id="outlined-search"
+                    label="Email"
+                    type="email"
+                    variant="outlined"
+                    placeholder="example@gmail.com"
+                    size="small"
+                    onChange={handleChange("email")}
+                    value={email}
+                  />
+                  <TextField
+                    className={classes.textField}
+                    id="outlined-password-input"
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    placeholder="Password"
+                    size="small"
+                    onChange={handleChange("password1")}
+                    value={password1}
+                  />
+                  <TextField
+                    className={classes.textField}
+                    id="outlined-password-input"
+                    label="Confirm Password"
+                    type="password"
+                    variant="outlined"
+                    size="small"
+                    placeholder="Confirm Password"
+                    onChange={handleChange("password2")}
+                    value={password2}
+                  />
+                  <div
+                    style={{
+                      marginTop: "1%",
+                    }}
+                  >
+                    <Typography variant="caption" align="center">
+                      <div className=" border-b text-center">
+                        <div className=" flex-1 leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
+                          Or Register with email{" "}
+                          <i className="fab fa-google w-10" />{" "}
+                          <i className="fab fa-facebook w-10-ml-2" />
+                        </div>
+                      </div>
+                    </Typography>
+                    {passwordError && (
+                      <Alert severity="error" className="mt-5">
+                        {passwordError}
+                      </Alert>
+                    )}
+                    <FormControlLabel
+                      value="end"
+                      control={<Checkbox color="primary" required />}
+                      style={{ marginTop: "5%" }}
+                      label={
+                        <div>
+                          <span>
+                            Yes, I understand and agree the Juniority.{" "}
+                          </span>
+                          <Link to={"/terms"} style={{ color: "#3CB4E6" }}>
+                            Terms & Conditions
+                          </Link>
+                        </div>
+                      }
+                      labelPlacement="end"
+                      className={classes.checkbox}
+                    />
+                    <Typography
+                      style={{
+                        textAlign: "start",
+                        width: "93%",
+                        margin: "auto",
+                        marginTop: "10%",
+                      }}
+                    >
+                      <Button type="submit" color="primary" variant="contained">
+                        <i className="fas fa-user-plus fa 1x w-6  -ml-2" />{" "}
+                        <span className="ml-3">Get started </span>{" "}
+                      </Button>
+                    </Typography>
                   </div>
-                </Typography>
-                {passwordError && (
-                  <Alert severity="error" className="mt-5">
-                    {passwordError}
-                  </Alert>
-                )}
-                <FormControlLabel
-                  value="end"
-                  control={<Checkbox color="primary" required />}
-                  style={{ marginTop: "5%" }}
-                  label={
-                    <div>
-                      <span>Yes, I understand and agree the Juniority. </span>
-                      <Link to={"/terms"} style={{ color: "#3CB4E6" }}>
-                        Terms & Conditions
-                      </Link>
-                    </div>
-                  }
-                  labelPlacement="end"
-                  className={classes.checkbox}
-                />
-                <Typography
-                  style={{
-                    textAlign: "start",
-                    width: "93%",
-                    margin: "auto",
-                    marginTop: "10%",
-                  }}
-                >
-                  <Button type="submit" color="primary" variant="contained">
-                    <i className="fas fa-user-plus fa 1x w-6  -ml-2" />{" "}
-                    <span className="ml-3">Get started </span>{" "}
-                  </Button>
-                </Typography>
-              </div>
-            </form>
-          )}
-        </Grid>
-      </Grid>
-      {/* // ----------------------- */}
-      {/* <div
+                </form>
+              )}
+            </Grid>
+          </Grid>
+          {/* // ----------------------- */}
+          {/* <div
         className="flex-1 bg-indigo-100 text-center hidden lg:flex"
         style={{ backgroundImage: `url(${rocket})`, objectFit: "contain" }}
       >
@@ -242,9 +253,9 @@ const Register = () => {
         </div>
 
       </div> */}
-
-    </div>
+        </div>
+      )}
+    </>
   );
 };
-
 export default Register;
