09508870e53b2a0b68117ca8f3cf07b28c80662c
Walker Peredo <walkerperedo@gmail.com>
2021-04-03 01:24:39 -0400

    bug del profile arreglado y login con github ya esta funcionando

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { currentUserReducer } from './currentUser'
-import { selectedUserReducer } from './selectedUser'
-import { selectedUserPostsReducer } from './selectedUserPosts'
+import { configureStore } from "@reduxjs/toolkit";
+import { currentUserReducer } from "./currentUser";
+import { selectedUserReducer } from "./selectedUser";
+import { selectedUserPostsReducer } from "./selectedUserPosts";
 
-import userReducer from './userSlice'
+import userReducer from "./userSlice";
 
 export default configureStore({
-    reducer:{
-        user: userReducer,
-        currentUser: currentUserReducer,
-        selectedUser: selectedUserReducer,
-        selectedUserPosts: selectedUserPostsReducer
-    }
+  reducer: {
+    user: userReducer,
+    currentUser: currentUserReducer,
+    // selectedUser: selectedUserReducer,
+    selectedUserPosts: selectedUserPostsReducer,
+  },
 });
